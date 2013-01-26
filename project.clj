66a70cbe9c77c7a80548e769a85dba485c399120
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-01-26 11:26:01 +0530

    bump version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-jdbc-session "0.1.0-SNAPSHOT"
+(defproject ring-jdbc-session "0.1.0"
   :description "JDBC based HTTP session store for Ring (Clojure)"
   :url "https://github.com/kumarshantanu/ring-jdbc-session"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                                   [clj-dbcp            "0.8.0"]
                                   [com.h2database/h2   "1.3.170"]
                                   [mysql/mysql-connector-java "5.1.6"]
-                                  [postgresql/postgresql "9.1-901.jdbc4"]]}
+                                  [postgresql/postgresql      "9.1-901.jdbc4"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC3"]]}}
